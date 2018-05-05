04fb3eacb197f3e5377cdab772a6df2686cd817a
Kartik Vempati <kvempati@Kartiks-MacBook-Pro.local>
2018-05-04 21:50:04 -0700

    init textMod.js with skeleton

---
+++ textMod.js
@@ -0,0 +1,19 @@
+/**
+ * This file handles the logic for finding instances of 'Toronto' and replacing it with 'Lebronto'
+ * 
+ * 
+ * Property Groove Cruise, 2018
+ */
+
+const reToronto = [ '', '', '' ];
+
+const replace = () => {
+}
+
+const scraper = () => {
+    /**
+     * From the current page, grab teh DOM , and search for any matching toronto strings
+     */
+
+     return documentObj;
+}
